6e20615312811e423b1c4b1471b27d7b8b1fde17
suromark <47992489+suromark@users.noreply.github.com>
2020-11-05 10:10:53 +0100

    interrupt flow now has timerMUX and single point of return

--- src/interruptCodeESP32.h
+++ src/interruptCodeESP32.h
@@ -7,121 +7,114 @@
 
 void IRAM_ATTR oneCycle();
 
-hw_timer_t * timer = NULL;
+hw_timer_t *timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 void setUpInterruptForESP32()
 {
-     timer = timerBegin(0, 800, true); // 80 Mhz downscale to 100,000 ticks/second
-  timerAttachInterrupt(timer, &oneCycle, true);
-  timerAlarmWrite(timer, 10, true); // call every 10th timer step = 10k /second
-  timerAlarmEnable(timer);
+    timer = timerBegin(0, 800, true); // 80 Mhz downscale to 100,000 ticks/second
+    timerAttachInterrupt(timer, &oneCycle, true);
+    timerAlarmWrite(timer, 10, true); // call every 10th timer step = 10k /second
+    timerAlarmEnable(timer);
 }
 
 void IRAM_ATTR oneCycle()
 {
     static uint16_t closingin = 0;   // extra delay on final approach
     static uint16_t takingoff = 256; // extra delay during first steps
-    static unsigned long lastMicros = 0;
-    static unsigned long nowMicros = 0;
 
-// portENTER_CRITICAL_ISR(&timerMux); // ideally wrap the interrupt routine in this
-// portEXIT_CRITICAL_ISR(&timerMux);
+    portENTER_CRITICAL_ISR(&timerMux); // ideally wrap the interrupt routine in this
 
-/* 
-// if faking "interrupt" through repeated calls in the loop, 
-// then skip the processing if it's not yet time
-    nowMicros = micros();
-
-    if( lastMicros + 1000 > nowMicros ) {
-        return; // not yet time!
-    }
-
-    lastMicros = nowMicros;
-*/
-
-    if (enblStep == false) /* motor is disabled, abort early */
+    do
     {
-        takingoff = 256; // if the motor is on hold, prepare soft start
-        return;
-    }
 
-    tickerNow++; // increase the internal subdivider;
-
-    if (tickerNow == 1)
-    {
-        for (int i = 0; i < NUM_AXIS; i++)
+        if (enblStep == false) /* motor is disabled, abort early */
         {
-            digitalWrite(motor_pins[i].pin_step, LOW); // always pull down Step signal after 2 timer loop
+            takingoff = 256; // if the motor is on hold, prepare soft start
+            break;
         }
-    }
 
-    /* Do nothing if target is reached */
+        tickerNow++; // increase the internal subdivider;
 
-    if (stepstodo == 0)
-    {
-        enblStep = false;
-        targetReached = true;
-        return;
-    }
-    else
-    {
-        targetReached = false;
-    }
+        if (tickerNow == 1)
+        {
+            for (int i = 0; i < NUM_AXIS; i++)
+            {
+                digitalWrite(motor_pins[i].pin_step, LOW); // always pull down Step signal after 2 timer loop
+            }
+        }
 
-    /*
+        /* Do nothing if target is reached */
+
+        if (stepstodo == 0)
+        {
+            enblStep = false;
+            targetReached = true;
+            break;
+        }
+        else
+        {
+            targetReached = false;
+        }
+
+        /*
     */
 
-    /*
+        /*
         Time for another step calculation?
         */
 
-    if (tickerNow >= MIN_SAFE_SPEED + tickerLimit + (closingin >> 1) + (takingoff >> 5))
-    {
-
-        tickerNow = 0; // restart the counter
-
-        if (takingoff > 0)
+        if (tickerNow >= MIN_SAFE_SPEED + tickerLimit + (closingin >> 1) + (takingoff >> 5))
         {
-            takingoff--;
-        }
 
-        /* calculate the closingin additional delay 
+            tickerNow = 0; // restart the counter
+
+            if (takingoff > 0)
+            {
+                takingoff--;
+            }
+
+            /* calculate the closingin additional delay 
     for the next call as "the closer to target, the slower" 
     - works for both accel and decel, 
     and the bigger tickerLimit gets (slower motion), 
     the less closingin affects the speed */
 
-        if (stepstodo < 256)
-        {
-            closingin = (256 - stepstodo) >> 4;
-        }
-        else
-        {
-            closingin = 0;
-        }
-
-        /* target reached? */
-
-        if (stepstodo-- == 0)
-        {
-            enblStep = false;
-            targetReached = true;
-            return;
-        }
-
-        /* do the Bresenham */
-
-        for (int i = 0; i < NUM_AXIS; i++)
-        {
-            bresenham[i] -= steps[i];
-            if (bresenham[i] < 0) // time for a step
+            if (stepstodo < 256)
             {
-                bresenham[i] += stepsmax;                   // re-prime while considering underflow
-                currentpos[i] += steps_dir[i];              // the position change
-                digitalWrite(motor_pins[i].pin_step, HIGH); // the actual step signal
+                closingin = (256 - stepstodo) >> 4;
+            }
+            else
+            {
+                closingin = 0;
+            }
+
+            /* target reached? */
+
+            if (stepstodo-- == 0)
+            {
+                enblStep = false;
+                targetReached = true;
+                break;
+            }
+
+            /* do the Bresenham */
+
+            for (int i = 0; i < NUM_AXIS; i++)
+            {
+                bresenham[i] -= steps[i];
+                if (bresenham[i] < 0) // time for a step
+                {
+                    bresenham[i] += stepsmax;                   // re-prime while considering underflow
+                    currentpos[i] += steps_dir[i];              // the position change
+                    digitalWrite(motor_pins[i].pin_step, HIGH); // the actual step signal
+                }
             }
         }
-    }
+    } while (0);
+
+    portEXIT_CRITICAL_ISR(&timerMux);
+
     return;
 };
 
