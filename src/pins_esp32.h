601d360f66b8cdc83037cac8ee13b22d637ae511
suromark <47992489+suromark@users.noreply.github.com>
2020-11-12 11:00:16 +0100

    ESP32 tweaks

--- src/pins_esp32.h
+++ src/pins_esp32.h
@@ -12,13 +12,13 @@ extern uint8_t outPins[]; // make global
 uint8_t stickPins[] = {34, 35, 32}; // Mapping of inputs to Axis X, Y, Z
 extern uint8_t stickPins[];         // make global
 
-#define ANALOG_RANGE 4096
-#define MY_STICK_DEAD 128 // size of the no-motion deadband zone around the center; 
+#define ANALOG_RANGE 3750 // measured, adjust as you see fit
+#define MY_STICK_DEAD 160 // guestimate size of the no-motion deadband zone around the center; 
 
 /* input pins */
 
-#define PIN_ROTA_1 16
-#define PIN_ROTA_2 17
+#define PIN_ROTA_1 17
+#define PIN_ROTA_2 16
 
 #define PUSH_ENCODER 15   // encoder push button
 #define PUSH_SKIP 5    // skip to next target
